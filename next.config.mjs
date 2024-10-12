c22c2b4eff665b0ddd61ad0270b42250a023a6a4
Ernie Hsiung <e@erniehsiung.com>
2024-10-12 09:01:30 -0700

    feat: Enable React strict mode and SWC transforms in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      forceSwcTransforms: true, // Force using SWC instead of Babel
+    },
+  }
+  
+  module.exports = nextConfig
